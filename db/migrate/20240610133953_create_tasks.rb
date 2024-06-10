8db9ae5b182c424636090c6a48725af19d2c57a2
Aguss <agussfourcade@gmail.com>
2024-06-10 15:46:15 +0200

    Set up databases

---
+++ db/migrate/20240610133953_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :description
+      t.datetime :due_date
+      t.boolean :completed, default: false
+      t.integer :priority, default: 3
+      t.references :user, foreign_key: true # Esto asume que tienes una tabla de users
+      t.string :status, default: "pending"
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end
